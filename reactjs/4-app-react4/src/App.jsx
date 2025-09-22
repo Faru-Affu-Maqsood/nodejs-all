7da8ef90218967ce09e16bd054eff5de134252c2
Faru-Affu-Maqsood <motivecomputersedu@gmail.com>
2025-09-22 09:39:17 +0530

    Completed

--- reactjs/4-app-react4/src/App.jsx
+++ reactjs/4-app-react4/src/App.jsx
@@ -5,16 +5,39 @@ function App () {
       email: 'ahad@theonlineguru.com',
       location: 'Universe'
     },
+    {
+      username: 'Mohammed',
+      email: 'mohammed@theuniversalguru.com',
+      location: 'Mecca & Medina'
+    },
+    {
+      username: 'Maqsood',
+      email: 'maqsood@theonlineguru.com',
+      location: 'Guntur'
+    },
+    {
+      username: 'Farheen',
+      email: 'farheen@theonlineguru.com',
+      location: 'Guntur'
+    },
+    {
+      username: 'Afshaan',
+      email: 'afshaann@theonlineguru.com',
+      location: 'Guntur'
+    },
   ];
+
   return (
     <>
-      <ul>
-        {
-          numbers.map((n) => (
-            <li key={crypto.randomUUID()}>Num: {n}</li>
-          ))
-        }
-      </ul>
+      <main>
+        {usersInfo.map(({ username, email, location }) => (
+          <div key={crypto.randomUUID()} style={{ marginTop: '10px', borderBottom: '1px solid #f1f1f1' }}>
+            <p style={{ margin: '0' }}><span style={{ fontWeight: 'bolder' }}>Name</span>: {username}</p>
+            <p style={{ margin: '0' }}><span style={{ fontWeight: 'bolder' }}>Email</span>: {email}</p>
+            <p style={{ margin: '0', marginBottom: '10px' }}><span style={{ fontWeight: 'bolder' }}>Location</span>: {location}</p>
+          </div>
+        ))}
+      </main >
     </>
   );
 }
